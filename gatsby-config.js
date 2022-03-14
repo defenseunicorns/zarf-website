0dea26dc2d95a7dfe5053727200a68b17109c4d2
Michael Winberry <mike@defenseunicorns.com>
2022-03-14 09:56:28 -0700

    #1. UPDATE: lint, format scripts. ADD: lint:fix script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 /** @type {import('gatsby').GatsbyConfig} */
-const path = require("path");
+const path = require('path');
 // Get paths of Gatsby's required rules, which as of writing is located at:
 // https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
 // gatsby/src/utils/eslint-rules
 const gatsbyRequiredRules = path.join(
   process.cwd(),
-  "node_modules",
-  "gatsby",
-  "dist",
-  "utils",
-  "eslint-rules"
+  'node_modules',
+  'gatsby',
+  'dist',
+  'utils',
+  'eslint-rules'
 );
 module.exports = {
   siteMetadata: {
@@ -39,17 +39,17 @@ module.exports = {
       __key: 'images'
     },
     {
-      resolve: "gatsby-plugin-eslint",
+      resolve: 'gatsby-plugin-eslint',
       options: {
         // Gatsby required rules directory
         rulePaths: [gatsbyRequiredRules],
         // Default settings that may be ommitted or customized
-        stages: ["develop"],
-        extensions: ["js", "jsx", "ts", "tsx"],
-        exclude: ["node_modules", "bower_components", ".cache", "public"],
+        stages: ['develop'],
+        extensions: ['js', 'jsx', 'ts', 'tsx'],
+        exclude: ['node_modules', 'bower_components', '.cache', 'public']
         // Any additional eslint-webpack-plugin options below
         // ...
-      },
-    },
+      }
+    }
   ]
 };
