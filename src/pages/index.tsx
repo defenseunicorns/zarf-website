ce07bf122da8b063c7bacd169a861c98bcd40348
Michael Winberry <mike@defenseunicorns.com>
2022-03-15 12:06:20 -0700

    #6. FIX gatsby config. UPDATE theme with madeline colors. UPDATE index with snackbar for example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,27 +6,101 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { Snackbar } from '@mui/material';
+
+function SimpleSnackbar() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (
+    _event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <Button size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        mt: '10rem',
+        flexDirection: { xs: 'column', xl: 'row' }
+      }}
+    >
+      <Button
+        size="large"
+        sx={{ mx: 'auto' }}
+        variant="outlined"
+        onClick={handleClick}
+      >
+        Open simple snackbar
+      </Button>
+      <Button
+        size="large"
+        sx={{ mx: 'auto' }}
+        variant="outlined"
+        onClick={handleClick}
+      >
+        Open simple snackbar
+      </Button>
+      <Snackbar
+        open={open}
+        onClose={handleClose}
+        message="Note archived"
+        action={action}
+      />
+    </Box>
+  );
+}
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <SimpleSnackbar />
+    </>
   );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,17 +1,53 @@
-import { red, purple } from '@mui/material/colors';
-import { createTheme } from '@mui/material';
+import { createTheme, Theme } from '@mui/material';
+
+export const COLORS = Object.freeze({
+  primary: {
+    main: '#4ADEDE',
+    dark: '#44A4C2',
+    light: '#B0FFFF',
+    contrastText: '#000000',
+    shades: {
+      '4p': '#7BD5F514',
+      '8p': '#7BD5F529',
+      '12p': '#7BD5F51F',
+      '30p': '#7BD5F54D',
+      '50p': '#7BD5F580',
+      '100p': ''
+    }
+  },
+  text: {
+    primary: '#FFFFFF',
+    secondary: '#FFFFFFB2',
+    disabled: '#FFFFFF80'
+  },
+  action: {
+    active: '#FFFFFF8F',
+    hover: '#FFFFFF14',
+    selected: '#FFFFFF29',
+    disabled: '#FFFFFF4D',
+    disabledBackground: '#FFFFFF1F',
+    focus: '#FFFFFF1F'
+  },
+  divider: '#FFFFFF1F',
+  components: {
+    MuiSnackbarContent: {
+      backgroundColor: '#1F2F98'
+    }
+  }
+});
 
 // A custom theme for this app
-const theme = createTheme({
+const theme: Theme = createTheme({
   palette: {
-    primary: {
-      main: purple[600]
-    },
-    secondary: {
-      main: purple[600]
-    },
-    error: {
-      main: red.A400
+    ...COLORS
+  },
+  components: {
+    MuiSnackbarContent: {
+      styleOverrides: {
+        root: {
+          background: COLORS.components.MuiSnackbarContent.backgroundColor
+        }
+      }
     }
   }
 });
