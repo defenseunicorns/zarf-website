a343784e47842f10d4ab1b395412f10a1be643e9
mike-winberry <86802655+mike-winberry@users.noreply.github.com>
2022-03-23 18:34:44 -0700

    6 configure mui theme (#13)
    * #6. FIX gatsby config. UPDATE theme with madeline colors. UPDATE index with snackbar for example
    
    * #6. UPDATE src/theme with typography and theme colors. UPDATE pages/index with test components to ensure typography and colors are being applyied as expected. UPDATE plugins/../TopLayout.js with the fonts required for typography.
    
    * #6. UPDATE theme: extracted components into their own frozen object. Added background color and paper color (needs validation).

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,27 +6,156 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Snackbar
+} from '@mui/material';
+
+function BasicSelect() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
+  return (
+    <Box
+      sx={{
+        width: 'full',
+        maxWidth: 120,
+        marginLeft: 'auto',
+        marginRight: 'auto'
+      }}
+    >
+      <FormControl
+        variant="filled"
+        sx={{
+          minWidth: 120,
+          maxWidth: 200,
+          marginLeft: 'auto',
+          marginRight: 'auto'
+        }}
+      >
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
+
+function SimpleSnackbar() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (
+    _event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <Button size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        mt: '10rem',
+        flexDirection: { xs: 'column', xl: 'row' }
+      }}
+    >
+      <Button
+        size="large"
+        sx={{ mx: 'auto' }}
+        variant="contained"
+        onClick={handleClick}
+      >
+        Open simple snackbar
+      </Button>
+      <Button
+        size="small"
+        sx={{ mx: 'auto' }}
+        variant="contained"
+        onClick={handleClick}
+      >
+        Open simple snackbar
+      </Button>
+      <Snackbar
+        open={open}
+        onClose={handleClose}
+        message="Note archived"
+        action={action}
+      />
+    </Box>
+  );
+}
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h1" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Typography variant="h2" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Typography variant="h3" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <SimpleSnackbar />
+      <BasicSelect />
+    </>
   );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,18 +1,130 @@
-import { red, purple } from '@mui/material/colors';
-import { createTheme } from '@mui/material';
+import { createTheme, Theme } from '@mui/material';
+
+const fontWeights = {
+  medium: 500,
+  regular: 400
+};
+
+const fonts = {
+  roboto: 'Roboto',
+  mPlusRoundedFont: '"M PLUS Rounded 1c"'
+};
+
+export const PALLETE = Object.freeze({
+  primary: {
+    main: '#4ADEDE',
+    dark: '#44A4C2',
+    light: '#B0FFFF',
+    contrastText: '#000000'
+  },
+  background: {
+    default: '#10184C',
+    paper: '#10184C'
+  },
+  text: {
+    primary: '#FFFFFF',
+    secondary: '#FFFFFFB2',
+    disabled: '#FFFFFF80'
+  },
+  action: {
+    active: '#FFFFFF8F',
+    hover: '#FFFFFF14',
+    selected: '#FFFFFF29',
+    disabled: '#FFFFFF4D',
+    disabledBackground: '#FFFFFF1F',
+    focus: '#FFFFFF1F'
+  },
+  contrastThreshold: 1,
+  divider: '#FFFFFF1F'
+});
+
+export const COMPONENTS = Object.freeze({
+  MuiSnackbarContent: {
+    styleOverrides: {
+      root: {
+        background: '#1F2F98'
+      }
+    }
+  },
+  MuiTextField: {
+    styleOverrides: {
+      root: {
+        borderColor: '#FFFFFF6B'
+      }
+    }
+  }
+});
+
+export const TYPEOGRAPHY = Object.freeze({
+  fontFamily: 'Roboto',
+  h1: {
+    fontFamily: fonts.mPlusRoundedFont,
+    fontSize: 64,
+    letterSpacing: -1.5,
+    fontWeight: fontWeights.medium
+  },
+  h2: {
+    fontFamily: fonts.mPlusRoundedFont,
+    fontSize: 48,
+    letterSpacing: -0.5,
+    fontWeight: fontWeights.medium
+  },
+  h3: {
+    fontFamily: fonts.mPlusRoundedFont,
+    fontSize: 36,
+    letterSpacing: 0,
+    fontWeigth: fontWeights.regular
+  },
+  h4: {
+    fontFamily: fonts.mPlusRoundedFont,
+    fontSize: 24,
+    letterSpacing: 0.25,
+    fontWeigth: fontWeights.regular
+  },
+  h5: {
+    fontFamily: fonts.mPlusRoundedFont,
+    fontSize: 20,
+    letterSpacing: 0.15,
+    fontWeight: fontWeights.medium
+  },
+  h6: {
+    fontFamily: fonts.mPlusRoundedFont,
+    fontSize: 20,
+    letterSpacing: 0,
+    fontWeight: fontWeights.regular
+  },
+  subtitle1: {
+    fontFamily: fonts.roboto,
+    fontSize: 18,
+    letterSpacing: 0.15,
+    fontWeights: fontWeights.regular
+  },
+  subtitle2: {
+    fontFamily: fonts.roboto,
+    fontSize: 16,
+    letterSpacing: 0.1,
+    fontWeights: fontWeights.medium
+  },
+  body1: {
+    fontFamily: fonts.roboto,
+    fontSize: 18,
+    letterSpacing: 0.15,
+    fontWeights: fontWeights.regular
+  },
+  body2: {
+    fontFamily: fonts.roboto,
+    fontSize: 16,
+    letterSpacing: 0.15,
+    fontWeights: fontWeights.regular
+  }
+});
 
 // A custom theme for this app
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: purple[600]
-    },
-    secondary: {
-      main: purple[600]
-    },
-    error: {
-      main: red.A400
-    }
+const theme: Theme = createTheme({
+  palette: { ...PALLETE },
+  components: { ...COMPONENTS },
+  typography: {
+    ...TYPEOGRAPHY
   }
 });
 
