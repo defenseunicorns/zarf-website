89c88c764a008161ab312caed7c4c5114e5d871f
edengebrezgi <edengebrezgi@gmail.com>
2022-04-01 17:05:37 -0400

    added coming soon page

---
+++ src/pages/comingSoon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// styles
+const pageStyles = {
+  color: '#232129',
+  padding: '96px',
+  fontFamily: '-apple-system, Roboto, sans-serif, serif'
+};
+
+// markup
+const ComingSoon = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Coming Soon!</title>
+    </main>
+  );
+};
+
+export default ComingSoon;
