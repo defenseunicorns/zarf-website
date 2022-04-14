328bc152ef60013056db094dc6ec0b2ef3f7d0ef
Michael Winberry <mike@defenseunicorns.com>
2022-04-14 07:27:15 -0700

    #61. Update NavLogo to link to homepage

--- src/components/NavBar/NavLogo.tsx
+++ src/components/NavBar/NavLogo.tsx
@@ -3,17 +3,18 @@ import React, { ReactElement } from 'react';
 import { hideLarge, hideSmall } from '../../utils/display';
 import ZarfLogo from '../../assets/svg/zarf-logo.svg';
 import ZarfLogoSmall from '../../assets/svg/zarf-logo-small.svg';
+import { Link } from 'gatsby';
 
 function NavLogo(): ReactElement {
   return (
-    <>
+    <Link to="/">
       <Box sx={hideSmall}>
         <ZarfLogo />
       </Box>
       <Box sx={hideLarge}>
         <ZarfLogoSmall />
       </Box>
-    </>
+    </Link>
   );
 }
 
