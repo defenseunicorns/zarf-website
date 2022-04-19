ee5a965ec264b6a0b963d2eda1a71312e5d9dd5a
Michael Winberry <mike@defenseunicorns.com>
2022-04-19 14:48:29 -0700

    REMOVE coming-soon page and extracted into ReroutePage. ADD ReroutePage component to 404 and index. UPDATE redirects so home goes to /. UPDATE tabs and iconbutton hover state colors globally.

--- src/components/NavBar/NavTabs.tsx
+++ src/components/NavBar/NavTabs.tsx
@@ -1,29 +1,16 @@
-import { TabUnstyledTypeMap } from '@mui/base/TabUnstyled/TabUnstyledProps';
-import { OverridableComponent } from '@mui/material/OverridableComponent';
 import { NavLinkTabProps } from '../../interfaces/NavLink';
 import { Link as LocalLink } from '@reach/router';
 import { isLocalLink } from '../../utils/navLink';
 import React, { ReactElement } from 'react';
-import palette from '../../theme/palette';
-import styled from '@emotion/styled';
+import { TabUnstyled } from '@mui/base';
 import Tab from '@mui/material/Tab';
-
-const TabWithHoverState = styled(Tab)`
-  &:hover {
-    background-color: ${palette.action?.hover};
-  }
-`;
 
 // MUI Tab Components are not matching the api and are not allowing component in tab to be passed.
 // The unstyled component does not have this issue.
 // So we are converting to the UnstyledTab return type in order to bypass this issue.
 // https://github.com/mui/material-ui/issues/16846
 // https://mui.com/material-ui/guides/routing/#tabs
-const LocalTabWithHoverState =
-  TabWithHoverState as unknown as OverridableComponent<
-    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-    TabUnstyledTypeMap<Record<string, any>, 'button'>
-  >;
+const LocalTabWithHoverState = Tab as typeof TabUnstyled;
 
 function InternalNavTab(props: NavLinkTabProps): ReactElement {
   return (
@@ -32,7 +19,7 @@ function InternalNavTab(props: NavLinkTabProps): ReactElement {
 }
 
 function ExternalNavTab(props: NavLinkTabProps): ReactElement {
-  return <TabWithHoverState {...props} />;
+  return <Tab {...props} />;
 }
 
 function NavTab(props: NavLinkTabProps): ReactElement {
