ad316f0d41ede99edb86438f3750b06234354c16
Michael Winberry <mike@defenseunicorns.com>
2022-05-05 14:45:04 -0700

    Removed helmet from plugin and added links to the seo component

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -1,26 +1,12 @@
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../../src/theme/theme';
-import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 import * as React from 'react';
 
 export default function TopLayout(props) {
   return (
     <React.Fragment>
-      <Helmet>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
