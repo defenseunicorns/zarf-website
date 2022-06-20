ac90892dc39c2f34d82fcf1ee34ceec4fffb4187
Michael Winberry <mike@defenseunicorns.com>
2022-06-20 12:19:51 -0700

    #139. Playing with fade transition for FOUT fix

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,12 +1,18 @@
-import React, { ReactElement, PropsWithChildren } from 'react';
 import ZarfNav from './Sections/NavBar/ZarfNav';
 import getPathname from '../hooks/getPathname';
 import Footer from './Sections/Footer';
 import SiteHelmet from './SiteHelmet';
+import Fade from '@mui/material/Fade';
 import '../assets/css/blobz.min.css';
 import { Box } from '@mui/material';
 import '../assets/css/global.css';
 import { SeoProps } from './seo';
+import React, {
+  ReactElement,
+  PropsWithChildren,
+  useState,
+  useEffect,
+} from 'react';
 
 export interface PageLayoutProps {
   seoProps?: SeoProps;
@@ -14,22 +20,32 @@ export interface PageLayoutProps {
 
 function PageLayout(props: PropsWithChildren<PageLayoutProps>): ReactElement {
   const pathname = getPathname();
+  const [fade, setFade] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout((): void => setFade(true), 500);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
   return (
     <>
       <SiteHelmet {...props.seoProps} route={pathname.replace('/', '')} />
       <ZarfNav pathname={pathname} />
-      <Box
-        component="main"
-        sx={{
-          display: 'flex',
-          width: '100%',
-          flexDirection: 'column',
-          gap: '72px',
-        }}
-      >
-        {props.children}
-        <Footer pathname={pathname} />
-      </Box>
+      <Fade in={fade}>
+        <Box
+          component="main"
+          sx={{
+            display: 'flex',
+            width: '100%',
+            flexDirection: 'column',
+            gap: '72px',
+          }}
+        >
+          {props.children}
+          <Footer pathname={pathname} />
+        </Box>
+      </Fade>
     </>
   );
 }
